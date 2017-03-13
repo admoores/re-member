a7820d90f4b61fe8f0fbe44c58c390d75f8f80b6
Alex Moores <moores.alexd@gmail.com>
2017-03-13 18:36:06 -0400

    test

--- server/server.js
+++ server/server.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
 
+app.get('/', function(req, res) {
+  console.log('heard a GET request');
+  res.end();
+});
 
 app.listen(80, function() {
   console.log('listening on port 80');
