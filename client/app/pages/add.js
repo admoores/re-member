fc0b7930e3ff7924a6e542ed1f289a3bf7b93168
Alex Moores <moores.alexd@gmail.com>
2017-03-14 14:00:58 -0400

    commit to push to server

--- client/app/pages/add.js
+++ client/app/pages/add.js
@@ -3,12 +3,10 @@ console.log('loaded add.js');
 angular.module('remember.add', [])
 
 .controller('AddController', function ($scope, Add) {
-  // var testObj1 = {title: 'This is a new page', link: 'this is a link', description: 'this is a description', category: 'this is a category'};
-  // Add.addNew(testObj1);
+  var testObj1 = {title: 'This is a new page', link: 'this is a link', description: 'this is a description', category: 'this is a category'};
+  Add.addNew(testObj1);
   var testObj2 = {title: 'This is another new page', link: 'this is another link', description: 'this is another description', category: 'funstuff'};
   Add.addNew(testObj2);
-  setTimeout(1000, function() {
-    var testObj3 = {title: 'This is a third new page', link: 'this is a third link', description: 'this is a third description', category: 'funstuff'};
-    Add.addNew(testObj3);
-  });
+  var testObj3 = {title: 'This is a third new page', link: 'this is a third link', description: 'this is a third description', category: 'funstuff'};
+  Add.addNew(testObj3);
 });
