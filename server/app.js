bb96f510622b008bc32b64b856f42dbb9df4ae3d
Alex Moores <moores.alexd@gmail.com>
2017-03-13 22:34:21 -0400

    commit to push to server

--- server/app.js
+++ server/app.js
@@ -11,10 +11,11 @@ app.get('/api/resources', function(req, res) {
   console.log('get to /api/resources');
   var fullList;
   var getCategories = db.getAllCategories().then(function(categoryList) {
-    console.log(categoryList);
+    console.log('Got Category List!!');
     fullList.categories = categoryList;
   });
   var getResources = db.getAllResources().then(function(resourceList) {
+    console.log('Got Resource List!!');
     fullList.resources = resourceList;
   });
   Promise.all([getCategories, getResources]).then(function() {
