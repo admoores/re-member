52307a8e8dc74a09e1e5d0b2d67ea712befcb785
Alex Moores <moores.alexd@gmail.com>
2017-03-13 23:54:56 -0400

    commit to push to server

--- client/app/pages/resources.js
+++ client/app/pages/resources.js
@@ -2,7 +2,7 @@ console.log('loaded resources.js');
 
 angular.module('remember.resources', [])
 
-.controller('ResourcesController', function ($scope, Resources) {
+.controller('ResourceController', function ($scope, Resources) {
   $scope.test = 'this is some test info'
   $scope.test2 = Resources();
 });
