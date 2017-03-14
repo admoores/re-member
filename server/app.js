1d3c9d565a2e301477abc7dabd5227b4828b66ff
Alex Moores <moores.alexd@gmail.com>
2017-03-13 22:20:07 -0400

    added functionality to get messages

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var path = require('path');
+var db = require('/dbinit');
 
 
 var app = express();
@@ -7,7 +8,17 @@ var app = express();
 app.use(express.static(path.resolve('client')));
 
 app.get('/api/resources', function(req, res) {
-
+  var fullList;
+  var getCategories = db.Category.findAll().then(function(categoryList) {
+    fullList.categories = categoryList;
+  });
+  var getResources = db.Resource.findAll().then(function(resourceList) {
+    fullList.resources = resourceList;
+  });
+  Promise.all([getCategories, getResources]).then(function() {
+    res.json(fullList);
+    res.end();
+  })
 });
 
 module.exports = {
