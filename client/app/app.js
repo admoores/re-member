2644a03e39991725013f6a66fa605c7edda7a52b
Alex Moores <moores.alexd@gmail.com>
2017-03-14 19:19:01 -0400

    commit to push to server

--- client/app/app.js
+++ client/app/app.js
@@ -44,7 +44,7 @@ angular.module('remember', [
   };
   return attach;
 })
-.run(function($rootScope, $locaiton, $window) {
+.run(function($rootScope, $location, $window) {
   $rootScope.on('$routeChangeStart', function(evt, next, current) {
     var hasToken = !!$window.localStorage.getItem('com.remember');
     if (!hasToken && next.$$route && next.$$route.auth) {
